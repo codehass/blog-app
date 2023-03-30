e954bb1b9b3eaee02cd59be311696df49ee08624
codehass <hassan.elouardy06@gmail.com>
2023-03-30 14:41:39 +0000

    Add methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   belongs_to :author, class_name: 'User'
+
+  after_save :update_post_counter
+
+  def update_post_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def most_recent_comments
+    comments.last(5)
+  end
 end
