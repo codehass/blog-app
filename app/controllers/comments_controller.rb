d5a4dd27621116be54933fe6e8d9e10e24874b84
codehass <hassan.elouardy06@gmail.com>
2023-04-13 19:47:00 +0000

    Update comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,6 +20,19 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    # authorize! :destroy, @comment
+
+    if @comment.destroy
+      flash[:success] = 'Comment deleted successfully.'
+      redirect_to user_post_path(@comment.post.author, @comment.post)
+    else
+      flash[:error] = 'Failed to delete comment.'
+      redirect_to @comment.post
+    end
+  end
+
   private
 
   def comment_params
