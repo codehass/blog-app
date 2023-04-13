5de3b692dc6b81b9cdaad85efda9d98f9dd4c5a7
codehass <hassan.elouardy06@gmail.com>
2023-04-13 19:49:30 +0000

    Add nessessary changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,13 @@
 class Post < ApplicationRecord
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   belongs_to :author, class_name: 'User'
 
   after_save :update_post_counter
 
   validates :title, presence: true, length: { maximum: 250 }
+
+  after_destroy :update_post_counter
 
   def update_post_counter
     author.update(posts_counter: author.posts.all.count)
