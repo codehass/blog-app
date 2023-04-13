510aebf698fe2a2421dd71bb3d8c192a19c28aac
codehass <hassan.elouardy06@gmail.com>
2023-04-13 18:12:24 +0000

    Update ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
       user ||= User.new # guest user (not logged in)
-      if user.admin?
+      if user.role = "admin"
         can :manage, :all
       else
         can :manage, Post, user_id: user.id
